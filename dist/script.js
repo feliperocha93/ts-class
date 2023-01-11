c19d79ea364229e7fba2f4a2db83abe78970b9f2
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2023-01-10 22:10:23 -0300

    feat: create normalize function

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,12 @@
 import { fetchData } from "./fetchData.js";
+import { normalizarTransacao } from './normalize.js';
 async function handleData() {
     const data = await fetchData('https://api.origamid.dev/json/transacoes.json');
-    data?.forEach(item => {
-        console.log(item.Email);
+    if (!data)
+        return;
+    const transacoes = data.map(normalizarTransacao);
+    transacoes?.forEach(item => {
+        console.log(item.email);
     });
 }
 console.log(await handleData());
---
+++ src/normalize.ts
@@ -0,0 +1,43 @@
+type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
+type TransacaoStatus =
+  | "Paga"
+  | "Recusada pela operadora de cartão"
+  | "Agurdando pagamento"
+  | "Estornada";
+
+export interface TransacaoAPI {
+  Nome: string;
+  ID: number;
+  Data: string;
+  Status: TransacaoStatus;
+  Email: string;
+  ["Valor (R$)"]: string;
+  ["Forma de Pagamento"]: TransacaoPagamento;
+  ["Cliente Novo"]: number;
+}
+
+interface Transacao {
+  nome: string;
+  id: number;
+  data: string;
+  status: TransacaoStatus;
+  email: string;
+  moeda: string;
+  valor: number | null;
+  pagamento: TransacaoPagamento;
+  novo: boolean;
+}
+
+export function normalizarTransacao(transacao: TransacaoAPI): Transacao {
+  return {
+      nome: transacao.Nome,
+      id: transacao.ID,
+      data: transacao.Data,
+      status: transacao.Status,
+      email: transacao.Email,
+      moeda: transacao['Valor (R$)'],
+      valor: 0,
+      pagamento: transacao['Forma de Pagamento'],
+      novo: Boolean(transacao['Cliente Novo']),
+  }
+}
