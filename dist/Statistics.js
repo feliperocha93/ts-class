439c2e32a05af44e5c9e5d2288709208e4704979
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2023-01-13 08:15:43 -0300

    feat: get count from status and payment

--- dist/Statistics.js
+++ dist/Statistics.js
@@ -1,15 +1,26 @@
+import { countBy } from "./countBy.js";
 function filterValue(transaction) {
     return transaction.valor !== null;
 }
 class Statistics {
     transactions;
     total;
+    payment;
+    status;
     constructor(transactions) {
         this.transactions = transactions;
         this.total = this.setTotal();
+        this.payment = this.setPayment();
+        this.status = this.setStatus();
     }
     setTotal() {
         return this.transactions.filter(filterValue).reduce((acc, item) => acc + item.valor, 0);
+    }
+    setPayment() {
+        return countBy(this.transactions.map(({ pagamento }) => pagamento));
+    }
+    setStatus() {
+        return countBy(this.transactions.map(({ status }) => status));
     }
 }
 export { Statistics };
--- src/Statistics.ts
+++ src/Statistics.ts
@@ -1,3 +1,4 @@
+import { countBy } from "./countBy.js";
 import { Transaction } from "./normalize";
 
 type TransacionValue = Transaction & { valor: number };
@@ -8,14 +9,26 @@ function filterValue(transaction: Transaction): transaction is TransacionValue {
 class Statistics {
   private transactions;
   total;
+  payment;
+  status;
 
   constructor(transactions: Transaction[]) {
     this.transactions = transactions;
     this.total = this.setTotal();
+    this.payment = this.setPayment();
+    this.status = this.setStatus();
   }
 
   private setTotal() {
     return this.transactions.filter(filterValue).reduce((acc, item) => acc + item.valor, 0);
+  }
+
+  private setPayment() {
+    return countBy(this.transactions.map(({pagamento}) => pagamento));
+  }
+
+  private setStatus() {
+    return countBy(this.transactions.map(({status}) => status));
   }
 }
 
