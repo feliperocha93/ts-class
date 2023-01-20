77a657538ac26c5113552c67476d80d664a2d41f
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2023-01-20 09:25:07 -0300

    feat: create Timeout class

---
+++ pratice-w-dom/src/Timeout.ts
@@ -0,0 +1,13 @@
+export default class Timeout {
+  id;
+  handler;
+
+  constructor(handler: TimerHandler, time: number) {
+    this.id = setTimeout(handler, time);
+    this.handler = handler;
+  }
+
+  clear() {
+    clearTimeout(this.id);
+  }
+}
